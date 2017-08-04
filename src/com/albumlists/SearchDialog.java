904cf8bb6c67e8973a461f11cce2bba6ef9f32d7
corey-smith <coreygsmith2013@gmail.com>
2017-08-03 20:45:09 -0500

    Added search criteria dropdown to search dialog.

--- src/com/albumlists/SearchDialog.java
+++ src/com/albumlists/SearchDialog.java
@@ -3,16 +3,24 @@ package com.albumlists;
 import android.app.Dialog;
 import android.content.Context;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class SearchDialog extends Dialog {
 
+	Context context;
+	
 	public SearchDialog(Context context) {
 		super(context);
+		this.context = context;
 	}
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		setContentView(R.layout.dialog_search);
-		
+		Spinner searchSpinner = (Spinner) findViewById(R.id.search_type_spinner);
+		ArrayAdapter<CharSequence> searchTypeAdapter = ArrayAdapter.createFromResource(this.context, R.array.search_types, android.R.layout.simple_spinner_dropdown_item);
+		searchTypeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		searchSpinner.setAdapter(searchTypeAdapter);
 	}
 }
