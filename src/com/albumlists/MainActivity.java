924fb047a77d6782922921509a26ec5529d70176
corey-smith <coreygsmith2013@gmail.com>
2017-08-03 19:36:47 -0500

    Added search icon.

--- src/com/albumlists/MainActivity.java
+++ src/com/albumlists/MainActivity.java
@@ -16,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
 		//get the toolbar out of main.xml and set it as the actionbar for the app
 		DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
 		Toolbar main_toolbar = (Toolbar) findViewById(R.id.main_toolbar);
@@ -41,7 +42,7 @@ public class MainActivity extends AppCompatActivity {
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
+		getMenuInflater().inflate(R.menu.main_menu, menu);
 		return true;
 	}
 
