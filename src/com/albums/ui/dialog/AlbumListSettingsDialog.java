7cf35bb95ba0f333551be3da964a10dd56b2c6e8
corey-smith <coreygsmith2013@gmail.com>
2017-08-13 14:21:30 -0500

    Refactoring save listener on album list settings save button.

--- src/com/albums/ui/dialog/AlbumListSettingsDialog.java
+++ src/com/albums/ui/dialog/AlbumListSettingsDialog.java
@@ -21,26 +21,18 @@ public class AlbumListSettingsDialog {
         this.albumList = albumList;
     }
 
+    /**
+     * Build dialog, add listeners
+     */
     public void show() {
         AlertDialog.Builder builder = new AlertDialog.Builder(baseAlbumActivity);
         builder.setTitle("List Settings");
         final EditText input = new EditText(baseAlbumActivity);
         input.setHint("Name");
+        input.setText(albumList.getName());
+        input.setSelection(input.getText().length());
         builder.setView(input);
-        builder.setPositiveButton("Save", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                listName = input.getText().toString();
-                if(listName.length() > 0) {
-                    albumList.setName(listName);
-                    try {
-                        baseAlbumActivity.refreshListSettings();
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        });
+        builder.setPositiveButton("Save", new SaveListener(input));
         builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
@@ -48,5 +40,30 @@ public class AlbumListSettingsDialog {
             }
         });
         builder.show();
+    }
+
+    /**
+     * Listener for save button, this will translate everything on the UI into their corresponding AlbumList fields
+     * As this is built out further, it should take in the Layout view rather than the individual TextEdit
+     */
+    public class SaveListener implements DialogInterface.OnClickListener {
+        EditText input;
+
+        public SaveListener(EditText input) {
+            this.input = input;
+        }
+
+        @Override
+        public void onClick(DialogInterface dialog, int which) {
+            listName = input.getText().toString();
+            if (listName.length() > 0) {
+                albumList.setName(listName);
+                try {
+                    baseAlbumActivity.refreshListSettings();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
